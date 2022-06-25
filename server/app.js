1c26a88294bf4a2affa888182b0f2338db84475f
Alexander <alex030t@stud.kea.dk>
2022-06-25 21:55:40 +0200

    Added helmet and express-rate-limit middleware

--- server/app.js
+++ server/app.js
@@ -4,6 +4,21 @@ import session from "express-session";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//middleware setup
+import helmet from "helmet";
+app.use(helmet());
+
+import rateLimit from 'express-rate-limit';
+
+const baseLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 500, // Limit 
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(baseLimiter);
+
 import path from "path";
 app.use(express.static(path.resolve("../client/public")));
 
